79319336dd98825cac7d048e0b221c7590febbb1
Mason Johnson <273-C0514274@users.noreply.gitlab.compsci.camosun.ca>
2024-09-28 13:33:27 -0700

    format flag data into standard FlagObject objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Game from './Game.jsx';
 import { fetchFlagData } from './api.js';
-import { FlagObject } from './flag.js';
 
+// flagData is an object in form: { code: { names: ["name"], src: "img_src" }, ... }
 const flagData = await fetchFlagData();
 
+
 export default function App() {
+  console.log(flagData);
+
   return (
     <>
       <Game model={flagData} />
--- src/api.js
+++ src/api.js
@@ -1,10 +1,20 @@
 // This file is responsible for fetching our flag/country data
+import { FlagObject } from './flag.js';
 
 export const fetchFlagData = async () => {
     try {
         const response = await fetch('/country_data.json');
         const data = await response.json();
-        return data;
+
+        // Format flagData into an array of FlagObject objects
+        const flagObjects = [];
+        for (const code in data) {
+            let names = data[code]["names"];
+            let src = data[code]["flag"];
+            flagObjects.push(new FlagObject(code, names, src));
+        }
+
+        return flagObjects;
     } catch(error) {
         console.log("Error fetching flag data:", error);
     }
