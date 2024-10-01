c114abbacc31c878ceebb7cf7f3102824869acda
Mason Johnson <273-C0514274@users.noreply.gitlab.compsci.camosun.ca>
2024-10-01 10:06:36 -0700

    formatting

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,13 @@ import FlagQuiz from './FlagQuiz.jsx';
 import { fetchFlagData } from './api.js';
 import { shuffleArray } from "./utils";
 
-// flagData is an array in form: [  { code: "code", names: ["name"], src: "img_src", continent: "continent", revealed: bool }, ... ]
+// flagData is an array of FlagData objects in form: [  { code: "code", names: ["name"], src: "img_src", continent: "continent", revealed: bool }, ... ]
 const flagData = await fetchFlagData();
 shuffleArray(flagData);
 shuffleArray(flagData);
 
 
 export default function App() {
-  // console.log(flagData);
-
   return (
     <>
       <FlagQuiz model={flagData} />
